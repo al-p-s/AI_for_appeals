d9f0f332e25635837e9ab44853b458b22de986dd
han7ter <sobmisha@Gmail.com>
2026-03-23 18:52:48 +0300

    новый датасет, уже 475 примеров

--- src/features/train_val_test_split.py
+++ src/features/train_val_test_split.py
@@ -1,13 +1,13 @@
 import json
 
-with open('../../data/training_data_bio.json', 'r', encoding='utf-8') as json_file:
+with open('../../data/dataset.json', 'r', encoding='utf-8') as json_file:
     data = json.load(json_file)
 
 with open('../../data/train_dataset.json', 'w', encoding='utf-8') as f1:
-    json.dump(data[:158], f1, ensure_ascii=False)
+    json.dump(data[:333], f1, ensure_ascii=False)
 
 with open('../../data/validation_dataset.json', 'w', encoding='utf-8') as f2:
-    json.dump(data[158:192], f2, ensure_ascii=False)
+    json.dump(data[333:404], f2, ensure_ascii=False)
 
 with open('../../data/test_dataset.json', 'w', encoding='utf-8') as f3:
-    json.dump(data[192:], f3, ensure_ascii=False)
+    json.dump(data[404:], f3, ensure_ascii=False)
