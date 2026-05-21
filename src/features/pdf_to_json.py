73c6bf8add26e23be80e79270e0cc8b163ebbbba
AzZR43L <alexandrpsarev5555@gmail.com>
2026-05-21 15:24:08 +0300

    100 test appeals extracted

--- src/features/pdf_to_json.py
+++ src/features/pdf_to_json.py
@@ -3,9 +3,9 @@ import json
 
 from src.features.text_extractor import pdf_extract
 
-directory = r'C:/appeals/appeals_350_sasha/appeals_350_sasha'
+directory = r'C:/appeals/appeals_100test_sasha'
 files = os.listdir(directory)
-json_path = "../../data/appeals_350_SASHKA.json"
+json_path = "../../data/appeals_100_test_sasha.json"
 
 all_appeals = []
 
